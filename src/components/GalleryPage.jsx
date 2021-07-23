739f6db282b8dc0fb5ef4f8967ae3416eeaa48fa
jonthefox <weissjon@gmail.com>
2021-07-23 21:46:35 +0300

    add 5 of mom's paintings

--- src/components/GalleryPage.jsx
+++ src/components/GalleryPage.jsx
@@ -4,16 +4,24 @@ import ImageGallery from "react-image-gallery";
 
 const images = [
   {
-    original: "https://picsum.photos/id/1018/1000/600/",
-    thumbnail: "https://picsum.photos/id/1018/250/150/",
+    original: "/paintings/001.jpg",
+    thumbnail: "/paintings/001.jpg",
   },
   {
-    original: "https://picsum.photos/id/1015/1000/600/",
-    thumbnail: "https://picsum.photos/id/1015/250/150/",
+    original: "/paintings/002.jpg",
+    thumbnail: "/paintings/002.jpg",
   },
   {
-    original: "https://picsum.photos/id/1019/1000/600/",
-    thumbnail: "https://picsum.photos/id/1019/250/150/",
+    original: "/paintings/003.jpg",
+    thumbnail: "/paintings/003.jpg",
+  },
+  {
+    original: "/paintings/004.jpg",
+    thumbnail: "/paintings/004.jpg",
+  },
+  {
+    original: "/paintings/005.jpg",
+    thumbnail: "/paintings/005.jpg",
   },
 ];
 
