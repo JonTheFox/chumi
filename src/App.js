db3d9c56a981477e99ac9ac14cc1f1f529ef9fa8
jonthefox <weissjon@gmail.com>
2021-07-23 20:48:19 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from "./logo.svg";
+import "./App.css";
+import GalleryPage from "./components/GalleryPage";
+
+function App() {
+  return (
+    <div className="App">
+      <GalleryPage></GalleryPage>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Chumi Weiss</p>
+        <a
+          className="App-link"
+          href=""
+          //target="_blank"
+          rel="noopener noreferrer"
+        >
+          Browse
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
